0f753d339cb63e8edbfe270c817ff6d3bcc92b3c
John Opaluwa <johnopaluwa@gmail.com>
2022-05-19 02:15:28 +0200

    add redux toolkit persit to local storage

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,24 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "redux/store";
+import { persistStore } from "reduxjs-toolkit-persist";
+import { PersistGate } from "reduxjs-toolkit-persist/integration/react";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const persistor = persistStore(store);
+
 root.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>
